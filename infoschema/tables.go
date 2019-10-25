64e05c32c7c9194fb4d3bc271cdfa8771621a850
Neil Shen <overvenus@gmail.com>
2019-10-25 22:10:59 +0800

    infoschema: initial effort for the plainboring project!
    Signed-off-by: Neil Shen <overvenus@gmail.com>

--- infoschema/tables.go
+++ infoschema/tables.go
@@ -18,10 +18,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"sort"
+	"strings"
 	"sync"
 	"time"
 
+	"github.com/BurntSushi/toml"
 	"github.com/pingcap/errors"
+	"github.com/pingcap/log"
 	"github.com/pingcap/parser/charset"
 	"github.com/pingcap/parser/model"
 	"github.com/pingcap/parser/mysql"
@@ -40,6 +43,7 @@ import (
 	"github.com/pingcap/tidb/util/pdapi"
 	"github.com/pingcap/tidb/util/set"
 	"github.com/pingcap/tidb/util/sqlexec"
+	"go.uber.org/zap"
 )
 
 const (
@@ -84,6 +88,10 @@ const (
 	tableTiKVRegionStatus                   = "TIKV_REGION_STATUS"
 	tableTiKVRegionPeers                    = "TIKV_REGION_PEERS"
 	tableTiDBServersInfo                    = "TIDB_SERVERS_INFO"
+
+	// config!
+	tableTiKVConfig = "TIKV_CONFIG"
+	tablePDConfig   = "PD_CONFIG"
 )
 
 type columnInfo struct {
@@ -605,6 +613,58 @@ var tableTiKVStoreStatusCols = []columnInfo{
 	{"UPTIME", mysql.TypeVarchar, 64, 0, nil, nil},
 }
 
+var tableTiKVConfigCols = []columnInfo{
+	{"STORE_ID", mysql.TypeLonglong, 21, 0, nil, nil},
+	{"ADDRESS", mysql.TypeVarchar, 64, 0, nil, nil},
+
+	{"RAFTSTORE.SYNC_LOG", mysql.TypeLonglong, 21, 0, nil, nil},
+	{"RAFTSTORE.STORE_POOL_THREAD", mysql.TypeLonglong, 21, 0, nil, nil},
+	{"RAFTSTORE.APPLY_POOL_THREAD", mysql.TypeLonglong, 21, 0, nil, nil},
+	{"RAFTSTORE.STORE_MAX_BATCH_SIZE", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.APPLY_MAX_BATCH_SIZE", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.HIBERNATE_REGIONS", mysql.TypeLonglong, 21, 0, nil, nil},
+
+	{"RAFTSTORE.RAFT_BASE_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+
+	{"RAFTSTORE.RAFT_LOG_GC_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.RAFT_LOG_GC_THRESHOLD", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.RAFT_LOG_GC_COUNT_LIMIT", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.RAFT_LOG_GC_SIZE_LIMIT", mysql.TypeVarchar, 64, 0, nil, nil},
+
+	{"RAFTSTORE.RAFT_ENTRY_CACHE_LIFE_TIME", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.RAFT_REJECT_TRANSFER_LEADER_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+
+	{"RAFTSTORE.SPLIT_REGION_CHECK_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.REGION_SPLIT_CHECK_DIFF", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.REGION_COMPACT_CHECK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.REGION_COMPACT_CHECK_STEP", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.REGION_COMPACT_MIN_TOMBSTONES", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.REGION_COMPACT_TOMBSTONES_PERCENT", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.LOCK_CF_COMPACT_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.LOCK_CF_COMPACT_BYTES_THRESHOLD", mysql.TypeVarchar, 64, 0, nil, nil},
+
+	{"RAFTSTORE.PD_HEARTBEAT_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.PD_STORE_HEARTBEAT_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+
+	{"RAFTSTORE.NOTIFY_CAPACITY", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.MESSAGES_PER_TICK", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.LEADER_TRANSFER_MAX_LOG_LAG", mysql.TypeLonglong, 64, 0, nil, nil},
+
+	{"RAFTSTORE.SNAP_MGR_GC_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.SNAP_GC_TIMEOUT", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.MAX_PEER_DOWN_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.MAX_LEADER_MISSING_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.ABNORMAL_LEADER_MISSING_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.PEER_STALE_STATE_CHECK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.SNAP_APPLY_BATCH_SIZE", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.CONSISTENCY_CHECK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.REPORT_REGION_FLOW_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.RAFT_STORE_MAX_LEADER_LEASE", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.MERGE_CHECK_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	{"RAFTSTORE.MERGE_MAX_LOG_GAP", mysql.TypeLonglong, 64, 0, nil, nil},
+	{"RAFTSTORE.CLEANUP_IMPORT_SST_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+}
+
 var tableAnalyzeStatusCols = []columnInfo{
 	{"TABLE_SCHEMA", mysql.TypeVarchar, 64, 0, nil, nil},
 	{"TABLE_NAME", mysql.TypeVarchar, 64, 0, nil, nil},
@@ -853,6 +913,207 @@ func dataForTiKVStoreStatus(ctx sessionctx.Context) (records [][]types.Datum, er
 		row[18].SetString(storeStat.Status.Uptime)
 		records = append(records, row)
 	}
+	return records, nil
+}
+
+func dataForTiKVConfig(ctx sessionctx.Context) (records [][]types.Datum, err error) {
+	tikvStore, ok := ctx.GetStore().(tikv.Storage)
+	if !ok {
+		return nil, errors.New("Information about TiKV store status can be gotten only when the storage is TiKV")
+	}
+	tikvHelper := &helper.Helper{
+		Store:       tikvStore,
+		RegionCache: tikvStore.GetRegionCache(),
+	}
+	_ = tikvHelper
+	// TODO: get config pd server
+
+	cfgReader := strings.NewReader(tikvConfigFixture)
+	tikvCfg := new(TikvConfig)
+	_, err = toml.DecodeReader(cfgReader, tikvCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	raftstoreCfg := tikvCfg.Raftstore
+
+	row := make([]types.Datum, len(tableTiKVConfigCols))
+	index := 0
+
+	// {"STORE_ID", mysql.TypeLonglong, 21, 0, nil, nil},
+	row[index].SetInt64(42)
+	index++
+
+	// {"ADDRESS", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString("tikv @ plainboring!")
+	index++
+
+	// {"SYNC_LOG", mysql.TypeLonglong, 21, 0, nil, nil},
+	if raftstoreCfg.SyncLog {
+		row[index].SetInt64(1)
+	} else {
+		row[index].SetInt64(0)
+	}
+	index++
+
+	// {"STORE_POOL_THREAD", mysql.TypeLonglong, 21, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.StorePoolSize)
+	index++
+
+	// {"APPLY_POOL_THREAD", mysql.TypeLonglong, 21, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.ApplyPoolSize)
+	index++
+
+	// {"STORE_MAX_BATCH_SIZE", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.StoreMaxBatchSize)
+	index++
+
+	// {"APPLY_MAX_BATCH_SIZE", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.ApplyMaxBatchSize)
+	index++
+
+	// {"HIBERNATE_REGIONS", mysql.TypeLonglong, 21, 0, nil, nil},
+	if raftstoreCfg.HibernateRegions {
+		row[index].SetInt64(1)
+	} else {
+		row[index].SetInt64(0)
+	}
+	index++
+
+	// {"RAFT_BASE_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RaftBaseTickInterval)
+	index++
+
+	// {"RAFT_LOG_GC_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RaftLogGCTickInterval)
+	index++
+
+	// {"RAFT_LOG_GC_THRESHOLD", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.RaftLogGCThreshold)
+	index++
+
+	// {"RAFT_LOG_GC_COUNT_LIMIT", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.RaftLogGCCountLimit)
+	index++
+
+	// {"RAFT_LOG_GC_SIZE_LIMIT", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RaftLogGCSizeLimit)
+	index++
+
+	// {"RAFT_ENTRY_CACHE_LIFE_TIME", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RaftEntryCacheLifeTime)
+	index++
+
+	// {"RAFT_REJECT_TRANSFER_LEADER_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RaftRejectTransferLeaderDuration)
+	index++
+
+	// {"SPLIT_REGION_CHECK_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.SplitRegionCheckTickInterval)
+	index++
+
+	// {"REGION_SPLIT_CHECK_DIFF", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RegionSplitCheckDiff)
+	index++
+
+	// {"REGION_COMPACT_CHECK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RegionCompactCheckInterval)
+	index++
+
+	// {"REGION_COMPACT_CHECK_STEP", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.RegionCompactCheckStep)
+	index++
+
+	// {"REGION_COMPACT_MIN_TOMBSTONES", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.RegionCompactMinTombstones)
+	index++
+
+	// {"REGION_COMPACT_TOMBSTONES_PERCENT", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.RegionCompactTombstonesPercent)
+	index++
+
+	// {"LOCK_CF_COMPACT_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.LockCfCompactInterval)
+	index++
+
+	// {"LOCK_CF_COMPACT_BYTES_THRESHOLD", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.LockCfCompactBytesThreshold)
+	index++
+
+	// {"PD_HEARTBEAT_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.PdHeartbeatTickInterval)
+	index++
+
+	// {"PD_STORE_HEARTBEAT_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.PdStoreHeartbeatTickInterval)
+	index++
+
+	// {"NOTIFY_CAPACITY", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.NotifyCapacity)
+	index++
+
+	// {"MESSAGES_PER_TICK", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.MessagesPerTick)
+	index++
+
+	// {"LEADER_TRANSFER_MAX_LOG_LAG", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.LeaderTransferMaxLogLag)
+	index++
+
+	// {"SNAP_MGR_GC_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.SnapMgrGCTickInterval)
+	index++
+
+	// {"SNAP_GC_TIMEOUT", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.SnapGCTimeout)
+	index++
+
+	// {"MAX_PEER_DOWN_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.MaxPeerDownDuration)
+	index++
+
+	// {"MAX_LEADER_MISSING_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.MaxLeaderMissingDuration)
+	index++
+
+	// {"ABNORMAL_LEADER_MISSING_DURATION", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.AbnormalLeaderMissingDuration)
+	index++
+
+	// {"PEER_STALE_STATE_CHECK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.PeerStaleStateCheckInterval)
+	index++
+
+	// {"SNAP_APPLY_BATCH_SIZE", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.SnapApplyBatchSize)
+	index++
+
+	// {"CONSISTENCY_CHECK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.ConsistencyCheckInterval)
+	index++
+
+	// {"REPORT_REGION_FLOW_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.ReportRegionFlowInterval)
+	index++
+
+	// {"RAFT_STORE_MAX_LEADER_LEASE", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.RaftStoreMaxLeaderLease)
+	index++
+
+	// {"MERGE_CHECK_TICK_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.MergeCheckTickInterval)
+	index++
+
+	// {"MERGE_MAX_LOG_GAP", mysql.TypeLonglong, 64, 0, nil, nil},
+	row[index].SetInt64(raftstoreCfg.MergeMaxLogGap)
+	index++
+
+	log.Info("tikv config", zap.Int("index", index), zap.Int("row", len(row)))
+	// {"CLEANUP_IMPORT_SST_INTERVAL", mysql.TypeVarchar, 64, 0, nil, nil},
+	row[index].SetString(raftstoreCfg.CleanupImportSstInterval)
+	index++
+
+	records = append(records, row)
 	return records, nil
 }
 
@@ -1869,6 +2130,8 @@ var tableNameToColumns = map[string][]columnInfo{
 	tableTiKVRegionStatus:                   tableTiKVRegionStatusCols,
 	tableTiKVRegionPeers:                    tableTiKVRegionPeersCols,
 	tableTiDBServersInfo:                    tableTiDBServersInfoCols,
+
+	tableTiKVConfig: tableTiKVConfigCols,
 }
 
 func createInfoSchemaTable(handle *Handle, meta *model.TableInfo) *infoschemaTable {
@@ -1974,6 +2237,9 @@ func (it *infoschemaTable) getRows(ctx sessionctx.Context, cols []*table.Column)
 		fullRows, err = dataForTikVRegionPeers(ctx)
 	case tableTiDBServersInfo:
 		fullRows, err = dataForServersInfo()
+
+	case tableTiKVConfig:
+		fullRows, err = dataForTiKVConfig(ctx)
 	}
 	if err != nil {
 		return nil, err
